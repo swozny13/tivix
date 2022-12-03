c0d039b207feb4fda4ac24afc8fb997be19275db
Sebastian Woźniak <s.wozniak@merixstudio.com>
2022-12-03 23:04:07 +0100

    Registered url

--- src/user/auth_urls.py
+++ src/user/auth_urls.py
@@ -6,7 +6,10 @@ from rest_framework_simplejwt.views import (
     TokenViewBase,
 )
 
+from user.api.views import RegisterView
+
 urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
     path("jwt/create/", TokenObtainPairView.as_view(), name="jwt-create"),
     path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt-refresh"),
     path("jwt/verify/", TokenVerifyView.as_view(), name="jwt-verify"),
