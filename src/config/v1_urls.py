5d1818ec57d6f0aa99ca5fe893b7868f428fddfe
Sebastian Woźniak <s.wozniak@merixstudio.com>
2022-12-04 19:00:15 +0100

    Extended v1_urls

--- src/config/v1_urls.py
+++ src/config/v1_urls.py
@@ -2,4 +2,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("auth/", include("user.auth_urls")),
+    path("category/", include("category.urls")),
+    path("budget/", include("budget.urls")),
 ]
