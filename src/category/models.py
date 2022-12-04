2c302ecf85e4a7c0fc7b822e683a6a1fe8ee711b
Sebastian Woźniak <s.wozniak@merixstudio.com>
2022-12-04 18:54:40 +0100

    Category __str__ representation

--- src/category/models.py
+++ src/category/models.py
@@ -8,3 +8,6 @@ class Category(UuidMixin):
     name = CharField(_("Name"), max_length=100, unique=True)
     created_date = DateTimeField(_("Created date"), auto_now_add=True)
     updated_date = DateTimeField(_("Updated date"), auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
